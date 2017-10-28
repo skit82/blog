9f1480e17e00f9de57360787167fad502d3b6c45
tyuba4 <pojarkov@tut.by>
2017-10-28 14:48:49 +0100

    Made changes in index.php to the correct operation of the plugin Falanster Events

--- index.php
+++ index.php
@@ -63,8 +63,9 @@ get_header(); ?>
                              * called content-___.php (where ___ is the Post Format name) and that will be used instead.
                              */
                             get_template_part( 'content', get_post_format() );
+                            echo get_post_meta($post->ID, '_falanster_event', true);
                         ?>
-        
+                       
                     <?php endwhile; ?>
         
                     <?php readit_the_posts_navigation(); ?>
